10c936b5a05f32809aea17cd47af68b13c3b0970
sauci <sauciytyt@gmail.com>
2026-03-13 20:34:15 +0100

    fix pity user undefined

--- commands/joueur/pity.js
+++ commands/joueur/pity.js
@@ -1,5 +1,6 @@
 const { EmbedBuilder } = require("discord.js")
 const setsData = require("../../cards/sets.json")
+const { getUser } = require("../../systems/userSystem")
 
 const sets = Array.isArray(setsData) ? setsData : setsData.sets
 
@@ -8,16 +9,18 @@ module.exports = {
  name:"pity",
  description:"Voir tes pity par set",
 
- async execute(interaction,user){
+ async execute(interaction){
+
+  const user = getUser(interaction.user.id)
 
   if(!user.pity)
-   user.pity={}
+   user.pity = {}
 
   const lines=[]
 
   for(const set of sets){
 
-   const pity=user.pity[set.id]||{UR:0,SSR:0}
+   const pity = user.pity[set.id] || {UR:0,SSR:0}
 
    lines.push(
 `**${set.name}**
@@ -31,7 +34,10 @@ module.exports = {
    .setTitle(`🎴 Pity de ${interaction.user.username}`)
    .setDescription(lines.join("\n\n"))
 
-  interaction.reply({embeds:[embed]})
+  await interaction.reply({
+   embeds:[embed],
+   ephemeral:true
+  })
 
  }
 
