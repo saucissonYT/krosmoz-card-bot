05a4f271312306d757dbcab217da1b5b99571c2d
sauci <sauciytyt@gmail.com>
2026-03-14 17:48:25 +0100

    fix card registry and data workflow

--- systems/cardRegistry.js
+++ systems/cardRegistry.js
@@ -1,39 +1,73 @@
 const { data } = require("./dataManager")
 
-const cardsData = data.cards || []
+/* ---------------- BUILD REGISTRY ---------------- */
 
-const cards = Array.isArray(cardsData) ? cardsData : cardsData.cards || []
+function buildRegistry(){
 
-const cardsById = Object.fromEntries(
- cards.map(c => [String(c.id), c])
-)
+ const cardsData = data.cards || []
 
-const cardsBySet = {}
+ const cards = Array.isArray(cardsData)
+  ? cardsData
+  : cardsData.cards || []
 
-for(const card of cards){
+ const cardsById = Object.fromEntries(
+  cards.map(c => [String(c.id), c])
+ )
 
- if(!cardsBySet[card.set])
-  cardsBySet[card.set] = []
+ const cardsBySet = {}
 
- cardsBySet[card.set].push(card)
+ for(const card of cards){
+
+  if(!cardsBySet[card.set])
+   cardsBySet[card.set] = []
+
+  cardsBySet[card.set].push(card)
+
+ }
+
+ return {
+  cards,
+  cardsById,
+  cardsBySet
+ }
 
 }
 
+/* ---------------- GETTERS ---------------- */
+
 function getCard(id){
+
+ const { cardsById } = buildRegistry()
  return cardsById[String(id)]
+
 }
 
 function getCards(){
+
+ const { cards } = buildRegistry()
  return cards
+
 }
 
 function getCardsBySet(setId){
+
+ const { cardsBySet } = buildRegistry()
  return cardsBySet[setId] || []
+
 }
+
+function getCardsById(){
+
+ const { cardsById } = buildRegistry()
+ return cardsById
+
+}
+
+/* ---------------- EXPORT ---------------- */
 
 module.exports = {
  getCard,
  getCards,
  getCardsBySet,
- cardsById
+ getCardsById
 }
