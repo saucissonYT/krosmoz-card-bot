21a3361b2f7a284b0fdf37d0f22aad7b98916105
sauci <sauciytyt@gmail.com>
2026-03-20 15:16:58 +0100

    fix constant

--- systems/constants.js
+++ systems/constants.js
@@ -19,10 +19,22 @@ const FUSION_COST = {
   C:5, U:6, R:8, SR:10, HR:12, UR:15, S:20
 }
 
+const RARITY_COLOR = {
+  C:"#95a5a6",
+  U:"#2ecc71",
+  R:"#3498db",
+  SR:"#9b59b6",
+  HR:"#e74c3c",
+  UR:"#f1c40f",
+  S:"#ecf0f1",
+  SSR:"#ffcc00"
+}
+
 module.exports = {
   RARITY_EMOJI,
   RARITY_ORDER,
   RARITY_PRICE,
   SELL_PRICE,
-  FUSION_COST
+  FUSION_COST,
+  RARITY_COLOR
 }
